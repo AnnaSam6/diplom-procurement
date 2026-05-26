2fc17e3485de1b580be101839e533a60e031a5bd
AnnaSam6 <anna.vitaeva@inbox.ru>
2026-05-26 16:42:37 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/reset-password/', UserViewSet.as_view({'post': 'reset_password'}), name='reset_password'),
 ]
